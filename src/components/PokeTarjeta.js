ccc8104667cca621cf1a9c5c1f9a40beb8cd9ad9
Delibes <delibes.ats@gmail.com>
2024-09-09 22:20:24 -0600

    guardado break

--- src/components/PokeTarjeta.js
+++ src/components/PokeTarjeta.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Col, Card, CardBody, CardFooter, CardImg, Badge } from 'reactstrap';
 
- const PokeTarjeta = () => {
+
+ const PokeTarjeta = (params) => {
+  const [pokemon, setPokemon] = useState({});
+  const [imagen, setImagen] = useState('');
+  const [cardClass, setCardClass] = useState('d-none');
+  const [LoadClass, setLoadClass] = useState('');
+
+  useEffect(() =>{
+    getPokemon();
+  },[]);
+
+const getPokemon = async () => {
+  const liga = params.poke.url;
+  axios.get(liga).then( async(response) => {
+    const respuesta = response.data;
+    setPokemon(respuesta);
+  })
+}
+  
+
   return (
-    <div>PokeTarjeta</div>
+    <Col sm='4' lg='3' className='mb-3'>
+      <Card>
+        <CardImg src={imagen} className='p-2' />
+      </Card>
+    </Col>
   )
 }
 
