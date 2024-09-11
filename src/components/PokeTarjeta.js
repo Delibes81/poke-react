0c364dc7efb4be00d3b8f978da84c353b4e39a11
Delibes <delibes.ats@gmail.com>
2024-09-10 21:01:37 -0600

    poketarjeta ok pokebola img load ok y carga de imagen ok

--- src/components/PokeTarjeta.js
+++ src/components/PokeTarjeta.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Col, Card, CardBody, CardFooter, CardImg, Badge } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
  const PokeTarjeta = (params) => {
@@ -18,14 +19,33 @@ const getPokemon = async () => {
   axios.get(liga).then( async(response) => {
     const respuesta = response.data;
     setPokemon(respuesta);
+    if(respuesta.sprites.other.dream_world.front_default !== null){
+      setImagen(respuesta.sprites.other.dream_world.front_default)
+    } else {
+      setImagen(respuesta.sprites.oyher['official-artwork'].front_default)
+    }
+    setCardClass('');
+    setLoadClass('d-none');
   })
 }
   
 
   return (
     <Col sm='4' lg='3' className='mb-3'>
-      <Card>
-        <CardImg src={imagen} className='p-2' />
+      <Card className={'shadow border-4 border-warning '+LoadClass}>
+        <CardImg src='/pokebolaload.gif' height="200" className='p-3'></CardImg>
+      </Card>
+      <Card className={'shadow border-4 border-warning '+cardClass}> 
+        <CardImg src={imagen} height='150' className='p-2'  />
+        <CardBody className='text-center'>
+          <Badge pill color='danger'># {pokemon.id}</Badge>
+          <label className='fs-4 text-capitalize'>{pokemon.name}</label>
+          <CardFooter className='bg-warning square bg-primary rounded-3'>
+            <Link className='btn btn-dark'>
+            <i className='fa-solid fa-info-circle'></i> Detalle
+            </Link>
+          </CardFooter>
+        </CardBody>
       </Card>
     </Col>
   )
